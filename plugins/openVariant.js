2ce2c591047549baa8b8fb506c542069335069de
Bruno Vidal <brunokasprik@gmail.com>
2023-02-06 16:05:21 -0300

    last commit

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -1,10 +1,8 @@
-const plugin = require("tailwindcss/plugin")
+const plugin = require("tailwindcss/plugin");
 
+const openVariant = plugin(function ({ addVariant }) {
+  addVariant("group-open", ":merge(.group).open &");
+  addVariant("peer-open", ":merge(.peer).open ~ &");
+});
 
-const opeVariant = plugin(function ({ addVariant }){
-  addVariant("group-open", ":merge(.group).open &")
-
-
-})
-
-module.exports = opeVariant
+module.exports = openVariant;
